d8febef59f95ede14c77c06306fdac701e18a4a4
Minhpng <92564285+Lightrose142@users.noreply.github.com>
2022-11-04 19:40:13 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Button from "./component/Button";
 function App() {
   return (
     <div style={{ 'padding': '3em' }}>
-
-      <Button primary rounded>Click me!</Button>
-      <Button rounded>Click me!</Button>
     </div>
   );
 }
