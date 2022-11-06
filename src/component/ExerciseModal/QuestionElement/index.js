91c1661fb37d0c492054b51344143b4f9a8e1958
Minhpng <92564285+Lightrose142@users.noreply.github.com>
2022-11-07 00:02:08 +0700

    no message

--- src/component/ExerciseModal/QuestionElement/index.js
+++ src/component/ExerciseModal/QuestionElement/index.js
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { useRef, useState } from "react";
 
 import Button from "../../Button";
 import styles from "./QuestionElement.module.scss"
@@ -6,8 +7,16 @@ import QuestionResult from "../QuestionResult/";
 
 function QuestionElement({ currentQuestion, exercisePackage }) {
     let answer = exercisePackage[currentQuestion].answer
+    const [checkAnswer, setCheckAnswer] = useState(null)
 
-    let isChecked, isRight
+    const userInputRef = useRef()
+
+    const handSubmit = () => {
+        if (userInputRef.current.value === answer) {
+            setCheckAnswer(true)
+        }
+    }
+
     return (
         <div className={clsx(styles.wrapper)}>
             <div className={clsx(styles.questionNumber)}>
@@ -15,12 +24,12 @@ function QuestionElement({ currentQuestion, exercisePackage }) {
             </div>
             <div className={clsx(styles.questionTitle)}>{exercisePackage[currentQuestion].questionName}</div>
             <div className={clsx(styles.userInput)}>
-                <input type="text" placeholder="Dịch câu trên sang tiếng Anh..." rows="4" />
+                <input ref={userInputRef} type="text" placeholder="Dịch câu trên sang tiếng Anh..." rows="4" />
                 <p className={clsx(styles.userInputCta)}>Gõ câu trả lời của bạn vào ô trên và bấm Nộp bài!</p>
             </div>
-            {isChecked && <QuestionResult />}
+            {/* {isChecked && <QuestionResult />} */}
             <div className={clsx(styles.btnWrapper)}>
-                <Button className={clsx(styles.submitAnwswer)}>Kiểm tra</Button>
+                <Button onClick={handSubmit} className={clsx(styles.submitAnwswer)}>Kiểm tra</Button>
                 <Button btnNoBg>Tôi muốn bỏ qua và học thử ngay</Button>
             </div>
         </div >
