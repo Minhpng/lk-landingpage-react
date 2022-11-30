540742abd54755de2f87be95529d113e71680140
Minhpng <92564285+Lightrose142@users.noreply.github.com>
2022-11-30 17:21:24 +0700

    add

--- src/component/ExerciseModal/QuestionElement/index.js
+++ src/component/ExerciseModal/QuestionElement/index.js
@@ -1,12 +1,12 @@
 import clsx from "clsx";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 import Button from "../../Button";
-import styles from "./QuestionElement.module.scss"
 import QuestionResult from "../QuestionResult/";
+import styles from "./QuestionElement.module.scss";
 
-import bellRight from '../../../assets/audio/bell-right.mp3'
-import bellWrong from '../../../assets/audio/bell-wrong.mp3'
+import bellRight from '../../../assets/audio/bell-right.mp3';
+import bellWrong from '../../../assets/audio/bell-wrong.mp3';
 
 const rightSound = new Audio(bellRight)
 const wrongSound = new Audio(bellWrong)
@@ -15,27 +15,29 @@ const wrongSound = new Audio(bellWrong)
 const exercisePackage = [
     {
         questionName: 'Tôi đi học bằng xe đạp mỗi ngày',
-        answers: [
-            'I go to school by bike every day'
-        ],
+        answers: ['I go to school by bike every day'],
+        explaination: 'Lorem ipsum dolor sit amet consectetur. Fusce dolor enim sapien id vestibulum dui habitant sem donec. Viverra malesuada dictum quam adipiscing. Dignissim at vulputate nisl ultricies vitae massa. Lorem ipsum dolor sit amet consectetur. Fusce dolor enim sapien id vestibulum dui habitant sem donec. Viverra malesuada dictum quam adipiscing. Dignissim at vulputate nisl ultricies vitae massa. '
     },
     {
         questionName: 'Có gì đó trong mắt của tôi',
-        answers: ["There's something in my eyes", "There is something in my eyes"]
+        answers: ["There's something in my eyes", "There is something in my eyes"],
+        explaination: "“There is” dùng với một đại từ hay danh từ số ít để chỉ cái gì đó tồn tại hay xảy ra ở hiện tại. Đại từ “something” là “cái gì đó”, cái hay thứ mà bạn không biết. Dùng giới từ “in” để chỉ vị trí bên trong cái gì: “in my eye” là “trong mắt tôi”. There’s something in my eye! Có cái gì đó trong mắt tôi ấy! Dùng câu cảm thán, kết thúc bằng dấu chấm than, để truyền đạt một cảm xúc mạnh mẽ. Do có gì đó bay hay lọt vào mắt bạn một cách đột ngột nên bạn đã la lên do khó chịu hay hoảng loạn, chẳng hạn."
     },
     {
         questionName: 'Bạn cảm thấy thế nào?',
         answers: ["How are you feeling?",
-            "How do you feel?"
-        ]
+            "How do you feel?",],
+        explaination: 'Lorem ipsum dolor sit amet consectetur. Fusce dolor enim sapien id vestibulum dui habitant sem donec. Viverra malesuada dictum quam adipiscing. Dignissim at vulputate nisl ultricies vitae massa. Lorem ipsum dolor sit amet consectetur. Fusce dolor enim sapien id vestibulum dui habitant sem donec. Viverra malesuada dictum quam adipiscing. Dignissim at vulputate nisl ultricies vitae massa. '
     },
     {
         questionName: 'Bạn ổn chứ?',
-        answers: ["Are you alright?", "Are you ok?", "Are you okay?"]
+        answers: ["Are you alright?", "Are you ok?", "Are you okay?"],
+        explaination: 'Lorem ipsum dolor sit amet consectetur. Fusce dolor enim sapien id vestibulum dui habitant sem donec. Viverra malesuada dictum quam adipiscing. Dignissim at vulputate nisl ultricies vitae massa. Lorem ipsum dolor sit amet consectetur. Fusce dolor enim sapien id vestibulum dui habitant sem donec. Viverra malesuada dictum quam adipiscing. Dignissim at vulputate nisl ultricies vitae massa. '
     },
     {
         questionName: 'Tôi ổn, cảm ơn',
-        answers: ["I am fine, thanks", "I'm fine, thanks"]
+        answers: ["I am fine, thanks", "I'm fine, thanks"],
+        explaination: 'Lorem ipsum dolor sit amet consectetur. Fusce dolor enim sapien id vestibulum dui habitant sem donec. Viverra malesuada dictum quam adipiscing. Dignissim at vulputate nisl ultricies vitae massa.'
     },
 ]
 
@@ -45,10 +47,12 @@ function QuestionElement({ action }) {
     const [currentQuestion, setCurrentQuestion] = useState(0)
     const [isSubmited, setSubmit] = useState(false)
     const [isAnswerRight, setCheckAnswer] = useState(null)
+    const [answer, setAnswer] = useState('')
 
     const [userInput, setUserInput] = useState('')
 
-    let answer = exercisePackage[currentQuestion].answers
+    const explanation = exercisePackage[currentQuestion].explaination
+
     const userInputRef = useRef()
 
     const eliminateSpecialChar = (strings) => {
@@ -64,8 +68,17 @@ function QuestionElement({ action }) {
         setSubmit(true)
         userInputRef.current.disabled = true
 
-        const newAnswers = answer.map(item => eliminateSpecialChar(item.toLowerCase().trim()))
+        const newAnswers = exercisePackage[currentQuestion].answers.map(item => eliminateSpecialChar(item.toLowerCase().trim()))
         const isRight = newAnswers.indexOf(userAnswer) !== -1
+        setAnswer(() => {
+            const answer = exercisePackage[currentQuestion].answers[newAnswers.indexOf(userAnswer)]
+            if (answer && answer !== -1) {
+                return answer
+            } else {
+                console.log(exercisePackage[currentQuestion].answers[0])
+                return exercisePackage[currentQuestion].answers[0]
+            }
+        })
         action.setCheckAnswer({
             isRight: isRight,
             isWrong: !isRight
@@ -118,9 +131,9 @@ function QuestionElement({ action }) {
                     rows="4"
                     onKeyUp={(e) => { !isSubmited && e.key === 'Enter' && handleSubmit() }}
                     onChange={(e) => { handleInput(e) }} value={userInput} />
-                <p className={clsx(styles.userInputCta)}>Gõ câu trả lời của bạn vào ô trên và bấm Nộp bài!</p>
+                {!isSubmited ? <p className={clsx(styles.userInputCta)}>Gõ câu trả lời của bạn vào ô trên và bấm Nộp bài!</p> : null}
             </div>
-            {isSubmited && <QuestionResult isRight={isAnswerRight} answer={answer[0]} />}
+            {isSubmited && <QuestionResult isRight={isAnswerRight} answer={answer} explanation={explanation} />}
             <div className={clsx(styles.btnWrapper)}>
                 {isSubmited
                     ? <Button onClick={showNextQuestion} className={clsx(styles.submitAnwswer)}>
--- src/component/ExerciseModal/QuestionResult/index.js
+++ src/component/ExerciseModal/QuestionResult/index.js
@@ -3,17 +3,30 @@ import clsx from 'clsx';
 import styles from './QuestionResult.module.scss';
 
 
-function QuestionResult({ isRight, answer }) {
+function QuestionResult({ isRight, answer, explanation }) {
     let element
+
+    console.log(answer);
 
 
     if (isRight) {
-        element = <p className={clsx(styles.answerResult)}>Đúng rồi! Hay quá!</p>
+        element = (<>
+            <p className={clsx(styles.answerResult)}>Đúng rồi! Hay quá!</p>
+            <div className={clsx(styles.resultWrapper)}>
+                <div className={clsx(styles.desc)}>Đáp án:</div>
+                <div className={clsx(styles.answer)}>{answer}</div>
+                <div className={clsx(styles.desc)}>Giải thích:</div>
+                <div className={clsx(styles.explanation)}>{explanation}</div>
+            </div>
+        </>
+        )
     } else {
         element = (
             <div className={clsx(styles.resultWrapper)}>
                 <div className={clsx(styles.desc)}>Đáp án:</div>
                 <div className={clsx(styles.answer)}>{answer}</div>
+                <div className={clsx(styles.desc)}>Giải thích:</div>
+                <div className={clsx(styles.explanation)}>{explanation}</div>
             </div>
         )
     }
